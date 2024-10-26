671056391cfc7d61e62904dd4b80551c962e3634
Allan <135379484+AllMaciente@users.noreply.github.com>
2024-10-26 17:10:07 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
-# Lista de aplicativos para instalar. Adicione ou remova conforme necessário.
+#!/bin/bash
+
+# Lista de aplicativos comuns a todas as distribuições
 COMMON_APPS=(
     git
     curl
@@ -12,26 +14,30 @@ COMMON_APPS=(
     duf
 )
 
+# Lista de aplicativos específicos para Debian e derivados
 DEBIAN_APPS=(
     fd-find
 )
 
+# Lista de aplicativos específicos para Fedora e derivados
 FEDORA_APPS=(
     fd-find
     eza
 )
 
+# Lista de aplicativos específicos para Arch Linux e derivados
 ARCH_APPS=(
     fd
     lazygit
     eza
 )
 
+# Lista de aplicativos específicos para openSUSE e derivados
 OPENSUSE_APPS=(
     fd
 )
 
-# Função para detectar a distribuição
+# Função para detectar a distribuição Linux
 detect_distro() {
     if [ -f /etc/os-release ]; then
         . /etc/os-release
@@ -48,17 +54,24 @@ install_debian() {
     sudo apt update
     sudo apt install -y "${COMMON_APPS[@]}" "${DEBIAN_APPS[@]}"
     sudo apt update
-	sudo apt install -y software-properties-common
-        sudo add-apt-repository ppa:deadsnakes/ppa -y
-        sudo apt install -y python3 python3-pip
+    sudo apt install -y software-properties-common
+    sudo add-apt-repository ppa:deadsnakes/ppa -y
+    sudo apt install -y python3 python3-pip
+
+    # Instalação do Node.js
     curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
-        sudo apt install -y nodejs
+    sudo apt install -y nodejs
+
+    # Instalação do Fish Shell
     sudo apt install -y fish
+
+    # Instalação do LazyGit
     LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
     curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
     tar xf lazygit.tar.gz lazygit
     sudo install lazygit /usr/local/bin
-    sudo apt update
+
+    # Instalação do Eza
     sudo apt install -y gpg
     sudo mkdir -p /etc/apt/keyrings
     wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
@@ -73,12 +86,19 @@ install_fedora() {
     echo "Instalando pacotes para $PRETTY_NAME..."
     sudo dnf update -y
     sudo dnf install -y "${COMMON_APPS[@]}" "${FEDORA_APPS[@]}"
-	sudo dnf install -y fish
-     sudo dnf install -y python3 python3-pip
+    sudo dnf install -y fish
+    sudo dnf install -y python3 python3-pip
+
+    # Instalação do Node.js
     curl -fsSL https://rpm.nodesource.com/setup_current.x | sudo bash -
-        sudo dnf install -y nodejs
+    sudo dnf install -y nodejs
+
+    # Instalação do LazyGit
     sudo dnf copr enable atim/lazygit -y
     sudo dnf install lazygit
+
+    # Instalação do Eza
+    sudo dnf install -y eza
 }
 
 # Função para instalar pacotes no Arch Linux e derivados (ex: Manjaro)
@@ -86,9 +106,11 @@ install_arch() {
     echo "Instalando pacotes para $PRETTY_NAME..."
     sudo pacman -Syu --noconfirm
     sudo pacman -S --noconfirm "${COMMON_APPS[@]}" "${ARCH_APPS[@]}"
-	sudo pacman -S --noconfirm fish
-    sudo pacman -Syu --noconfirm python python-pip
-      sudo pacman -S --noconfirm nodejs npm
+    sudo pacman -S --noconfirm fish
+
+    # Instalação do Node.js
+    sudo pacman -S --noconfirm python python-pip
+    sudo pacman -S --noconfirm nodejs npm
 }
 
 # Função para instalar pacotes no openSUSE e derivados
@@ -96,71 +118,78 @@ install_opensuse() {
     echo "Instalando pacotes para $PRETTY_NAME..."
     sudo zypper refresh
     sudo zypper install -y "${COMMON_APPS[@]}" "${OPENSUSE_APPS[@]}"
-	sudo zypper install -y fish
+    sudo zypper install -y fish
     sudo zypper install -y python3 python3-pip
-     sudo zypper install -y nodejs npm
+
+    # Instalação do Node.js
+    sudo zypper install -y nodejs npm
     sudo zypper ar https://download.opensuse.org/repositories/devel:/languages:/go/openSUSE_Factory/devel:languages:go.repo
     sudo zypper ref && sudo zypper in lazygit
-    zypper ar https://download.opensuse.org/tumbleweed/repo/oss/ factory-oss
-    zypper in eza
+    sudo zypper ar https://download.opensuse.org/tumbleweed/repo/oss/ factory-oss
+    sudo zypper in eza
 }
 
+# Função para instalar pacotes de acordo com a distribuição detectada
 install_bydistro() {
-	case $DISTRO in
-	        ubuntu | debian | pop)
-	            install_debian
-	            ;;
-	        fedora | rhel | centos)
-	            install_fedora
-	            ;;
-	        arch | manjaro)
-	            install_arch
-	            ;;
-	        opensuse*)
-	            install_opensuse
-	            ;;
-	        *)
-	            echo "Distribuição não suportada: $DISTRO"
-	            exit 1
-	            ;;
-	    esac
-	
-	    echo "Instalação concluída!"
+    case $DISTRO in
+        ubuntu | debian | pop)
+            install_debian
+            ;;
+        fedora | rhel | centos)
+            install_fedora
+            ;;
+        arch | manjaro)
+            install_arch
+            ;;
+        opensuse*)
+            install_opensuse
+            ;;
+        *)
+            echo "Distribuição não suportada: $DISTRO"
+            exit 1
+            ;;
+    esac
+
+    echo "Instalação concluída!"
 }
+
+# Função para configurar o Fish Shell
 configure_fish() {
     echo "Configurando o Fish shell..."
-	curl -sS https://starship.rs/install.sh | sh
+    curl -sS https://starship.rs/install.sh | sh
 
-	URL="https://raw.githubusercontent.com/AllMaciente/setup-linux/refs/heads/main/config.fish"
-   
-    mkdir -p "~/.config/fish"
+    URL="https://raw.githubusercontent.com/AllMaciente/setup-linux/refs/heads/main/config.fish"
+    mkdir -p ~/.config/fish
+    wget -P ~/.config/fish "$URL"
 
-	wget -P "~/.config/fish" "$URL"
-  	
+    # Instalação do Fisher
+    curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+    fisher install PatrickF1/fzf.fish
+    fisher install jorgebucaran/autopair.fish
+
     echo "Configuração do Fish shell concluída!"
 }
+
+# Função para definir o Fish shell como padrão
 set_fish_as_default() {
     echo "Definindo o Fish shell como padrão..."
-    chsh -s $(which fish)
+    chsh -s "$(which fish)"
 }
-install_fish_plugins(){
-	fisher install PatrickF1/fzf.fish
-	fisher install jorgebucaran/autopair.fish
-	
+
+# Função para instalar pacotes CLI adicionais
+install_cli() {
+    pip install pipx
+    pipx install classifier
+    npm install -g tldr
 }
-install_cli(){
-	pip install pipx
-	pipx install classifier
-	npm install -g tldr
-}
+
 # Função principal
 main() {
     detect_distro
-	install_bydistro
-	configure_fish
-	set_fish_as_default
-	install_fish_plugins
-	install_cli
+    install_bydistro
+    configure_fish
+    set_fish_as_default
+    install_cli
 }
 
 # Executa a função principal
